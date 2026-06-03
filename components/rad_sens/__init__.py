27af94a1a41799904a8c6b27748868ff99cceb07
ananyevgv <81970899+ananyevgv@users.noreply.github.com>
2026-06-03 15:52:28 +0300

    Update __init__.py

--- components/rad_sens/__init__.py
+++ components/rad_sens/__init__.py
@@ -4,15 +4,18 @@ from esphome.components import i2c
 from esphome.const import CONF_ID, CONF_ADDRESS, CONF_UPDATE_INTERVAL
 
 DEPENDENCIES = ['i2c']
-AUTO_LOAD = ['sensor', 'binary_sensor', 'switch', 'number']
+AUTO_LOAD = ['sensor', 'binary_sensor', 'switch']
 
 rad_sens_ns = cg.esphome_ns.namespace('rad_sens')
 RadSensComponent = rad_sens_ns.class_('RadSensComponent', i2c.I2CDevice, cg.Component)
+
+CONF_SENSITIVITY = "sensitivity"
 
 CONFIG_SCHEMA = cv.Schema({
     cv.GenerateID(): cv.declare_id(RadSensComponent),
     cv.Optional(CONF_ADDRESS, default=0x66): cv.i2c_address,
     cv.Optional(CONF_UPDATE_INTERVAL, default='60s'): cv.update_interval,
+    cv.Optional(CONF_SENSITIVITY, default=105): cv.int_range(min=100, max=1100),
 }).extend(cv.COMPONENT_SCHEMA).extend(i2c.i2c_device_schema(0x66))
 
 async def to_code(config):
@@ -22,3 +25,6 @@ async def to_code(config):
     
     if CONF_UPDATE_INTERVAL in config:
         cg.add(var.set_update_interval(config[CONF_UPDATE_INTERVAL]))
+    
+    if CONF_SENSITIVITY in config:
+        cg.add(var.set_sensitivity(config[CONF_SENSITIVITY]))
