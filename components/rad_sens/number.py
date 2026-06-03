c1220389e0539e99ba083f29e52c8cac11095b69
ananyevgv <81970899+ananyevgv@users.noreply.github.com>
2026-06-03 15:42:10 +0300

    Update number.py

--- components/rad_sens/number.py
+++ components/rad_sens/number.py
@@ -9,7 +9,13 @@ DEPENDENCIES = ['rad_sens']
 
 CONFIG_SCHEMA = cv.Schema({
     cv.GenerateID(): cv.use_id(RadSensComponent),
-    cv.Optional("sensitivity"): number._NUMBER_SCHEMA,
+    cv.Optional("sensitivity"): number.NUMBER_SCHEMA.extend({
+        cv.Optional("min_value", default=100): cv.float_,
+        cv.Optional("max_value", default=1100): cv.float_,
+        cv.Optional("step", default=1): cv.float_,
+        cv.Optional("unit_of_measurement", default="imp/µR"): cv.string_strict,
+        cv.Optional("icon", default="mdi:tune"): cv.icon,
+    }),
 })
 
 async def to_code(config):
@@ -17,11 +23,16 @@ async def to_code(config):
     
     if "sensitivity" in config:
         sens_config = config["sensitivity"]
+        
+        # Создаём number
         num = await number.new_number(
             sens_config,
-            min_value=100,
-            max_value=1100,
-            step=1
+            min_value=sens_config["min_value"],
+            max_value=sens_config["max_value"],
+            step=sens_config["step"]
+        )
+        
+        cg.add(parent.set_sensitivity_number(num))
         )
         
         cg.add(parent.set_sensitivity_number(num))
