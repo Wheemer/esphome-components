f727ce49225bba219fa91a2d48d7fb55323881a2
ananyevgv <81970899+ananyevgv@users.noreply.github.com>
2026-06-03 16:29:58 +0300

    Create high_voltage_control.h

---
+++ components/rad_sens/switch/high_voltage_control.h
@@ -0,0 +1,19 @@
+#pragma once
+
+#include "esphome/components/switch/switch.h"
+#include "../rad_sens.h"
+
+namespace esphome {
+namespace rad_sens {
+
+class HighVoltageControl : public switch_::Switch, public Parented<RadSensComponent> {
+ public:
+  HighVoltageControl() = default;
+  void setup() override;
+
+ protected:
+  void write_state(bool state) override;
+};
+
+}  // namespace rad_sens
+}  // namespace esphome
