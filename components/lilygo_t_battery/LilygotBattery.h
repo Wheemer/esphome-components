0711a187045651b97be705a74028491640dd0d65
ananyevgv <81970899+ananyevgv@users.noreply.github.com>
2026-06-03 14:03:06 +0300

    Update LilygotBattery.h

--- components/lilygo_t_battery/LilygotBattery.h
+++ components/lilygo_t_battery/LilygotBattery.h
@@ -1,11 +1,13 @@
 #pragma once
+
 #include "esphome/core/component.h"
 #include "esphome/components/sensor/sensor.h"
 #include "esphome/core/hal.h"
 
 #ifdef USE_ESP32
-#include <driver/adc.h>
-#include "esp_adc_cal.h"
+#include "esp_adc/adc_oneshot.h"
+#include "esp_adc/adc_cali.h"
+#include "esp_adc/adc_cali_scheme.h"
 #endif
 
 namespace esphome {
@@ -13,17 +15,23 @@ namespace lilygo_t_battery {
 
 class LilygotBattery : public PollingComponent {
  public:
-  sensor::Sensor *voltage{nullptr};
-  sensor::Sensor *bus_voltage{nullptr};
-  sensor::Sensor *battery_level{nullptr};
-
-  void set_voltage_divider(float divider) { voltage_divider_ = divider; }
-  void set_enable_pin(GPIOPin *enable) { enable_pin_ = enable; }
-  void set_adc_sensor(sensor::Sensor *sensor) { adc_sensor_ = sensor; }
-  void set_voltage_sensor(sensor::Sensor *sensor) { voltage = sensor; }
-  void set_bus_voltage_sensor(sensor::Sensor *sensor) { bus_voltage = sensor; }
-  void set_battery_level_sensor(sensor::Sensor *sensor) { battery_level = sensor; }
+  // Сенсоры
+  sensor::Sensor *voltage_sensor_{nullptr};
+  sensor::Sensor *bus_voltage_sensor_{nullptr};
+  sensor::Sensor *battery_level_sensor_{nullptr};
   
+  // Настройка
+  void set_adc_sensor(sensor::Sensor *sensor) { adc_sensor_ = sensor; }
+  void set_enable_pin(GPIOPin *enable) { enable_pin_ = enable; }
+  void set_voltage_divider(float divider) { voltage_divider_ = divider; }
+  void set_reference_voltage(float voltage) { reference_voltage_ = voltage; }
+  
+  // Методы для установки сенсоров
+  void set_voltage_sensor(sensor::Sensor *sensor) { voltage_sensor_ = sensor; }
+  void set_bus_voltage_sensor(sensor::Sensor *sensor) { bus_voltage_sensor_ = sensor; }
+  void set_battery_level_sensor(sensor::Sensor *sensor) { battery_level_sensor_ = sensor; }
+
+  // Основные методы
   void setup() override;
   void update() override;
   void dump_config() override;
@@ -31,17 +39,18 @@ class LilygotBattery : public PollingComponent {
  protected:
   sensor::Sensor *adc_sensor_{nullptr};
   GPIOPin *enable_pin_{nullptr};
-  float voltage_divider_ = 6.0f;
-  float reference_voltage_ = 3.3f;
+  float voltage_divider_{6.0f};
+  float reference_voltage_{3.3f};
   
+  // Вспомогательные методы
   float read_adc_voltage_();
-  float calculate_battery_voltage_(float adc_voltage);
-  int calculate_battery_level_(float battery_voltage, bool is_usb_present);
-  bool is_usb_present_();
+  float calculate_usb_voltage_(float battery_voltage);
+  int calculate_battery_level_(float battery_voltage);
   
 #ifdef USE_ESP32
-  esp_adc_cal_characteristics_t adc_chars_;
-  bool adc_calibrated_ = false;
+  adc_oneshot_unit_handle_t adc_handle_{nullptr};
+  adc_cali_handle_t cali_handle_{nullptr};
+  bool adc_initialized_{false};
 #endif
 };
 
