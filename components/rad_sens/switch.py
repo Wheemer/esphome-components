d90c4704d90e26dcafc71bece3ee569e43ec7c22
ananyevgv <81970899+ananyevgv@users.noreply.github.com>
2026-06-03 16:22:13 +0300

    Update switch.py

--- components/rad_sens/switch.py
+++ components/rad_sens/switch.py
@@ -9,22 +9,22 @@ DEPENDENCIES = ['rad_sens']
 
 CONFIG_SCHEMA = cv.Schema({
     cv.GenerateID(): cv.use_id(RadSensComponent),
-    cv.Optional("hv_generator_switch"): switch.switch_schema(class_=switch.Switch),
-    cv.Optional("led_switch"): switch.switch_schema(class_=switch.Switch),
-    cv.Optional("low_power_switch"): switch.switch_schema(class_=switch.Switch),
+    cv.Optional("hv_generator_switch"): switch.switch_schema(),
+    cv.Optional("led_switch"): switch.switch_schema(),
+    cv.Optional("low_power_switch"): switch.switch_schema(),
 })
 
 async def to_code(config):
     parent = await cg.get_variable(config[CONF_ID])
     
     if "hv_generator_switch" in config:
-        sw = await switch.new_switch(config["hv_generator_switch"])
+        sw = await cg.get_variable(config["hv_generator_switch"]["id"])
         cg.add(parent.set_hv_generator_switch(sw))
     
     if "led_switch" in config:
-        sw = await switch.new_switch(config["led_switch"])
+        sw = await cg.get_variable(config["led_switch"]["id"])
         cg.add(parent.set_led_switch(sw))
     
     if "low_power_switch" in config:
-        sw = await switch.new_switch(config["low_power_switch"])
+        sw = await cg.get_variable(config["low_power_switch"]["id"])
         cg.add(parent.set_low_power_switch(sw))
