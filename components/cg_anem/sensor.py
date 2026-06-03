a7efe5d351f3ed3d8728a34eacc81c60df301d66
ananyevgv <81970899+ananyevgv@users.noreply.github.com>
2026-06-03 12:08:52 +0300

    Update sensor.py

--- components/cg_anem/sensor.py
+++ components/cg_anem/sensor.py
@@ -1,10 +1,9 @@
 import esphome.codegen as cg
 import esphome.config_validation as cv
 from esphome.components import i2c, sensor, binary_sensor
-
 from esphome.const import (
     CONF_ID,
-
+    CONF_DUCT,  # Добавлен импорт
     DEVICE_CLASS_TEMPERATURE,
     DEVICE_CLASS_WIND_SPEED,
     DEVICE_CLASS_FIRMWARE,
@@ -12,11 +11,11 @@ from esphome.const import (
     DEVICE_CLASS_VOLUME,
     DEVICE_CLASS_EMPTY,
     DEVICE_CLASS_PROBLEM,
-
     UNIT_CELSIUS,
     UNIT_WATT,
     UNIT_CUBIC_METER_PER_HOUR,
     UNIT_EMPTY,
+    UNIT_METER_PER_SECOND,  # Добавлен
     STATE_CLASS_MEASUREMENT,
     ICON_CHIP,
     ICON_WEATHER_WINDY,
@@ -24,7 +23,8 @@ from esphome.const import (
     ICON_HEATING_COIL,
     ENTITY_CATEGORY_DIAGNOSTIC
 )
-CONF_DUCT = "duct"
+
+# Константы конфигурации
 CONF_AMBIENT_TEMPERATURE = "ambient_temperature"
 CONF_HOTEND_TEMPERATURE = "hotend_temperature"
 CONF_HEAT_POWER = "heat_power"
@@ -36,11 +36,9 @@ CONF_MAX_AIR_FLOW_RATE = "max_air_flow_rate"
 CONF_STATUS_UP = "status_up"
 CONF_STATUS_OV = "status_ov"
 CONF_STATUS_WDT = "status_wdt"
-UNIT_METER_PER_SECOND = "m/s"
-UNIT_COUNTS_PER_SQUARED_CENTIMETER = "cm²"
 
 DEPENDENCIES = ["i2c"]
-CODEOWNERS = ["@ananevgv"]
+CODEOWNERS = ["@ananyevgv"]  # Исправлен неймспейс
 
 cg_anem_ns = cg.esphome_ns.namespace("cg_anem")
 CGAnemComponent = cg_anem_ns.class_(
@@ -48,123 +46,131 @@ CGAnemComponent = cg_anem_ns.class_(
 )
 
 CONFIG_SCHEMA = (
-    cv.Schema(
-        {
-            cv.Optional(CONF_DUCT, default=91.6): cv.float_range(min=1, max=1000),
-            cv.GenerateID(): cv.declare_id(CGAnemComponent),
-            cv.Optional(CONF_AMBIENT_TEMPERATURE): sensor.sensor_schema(
-                icon = ICON_THERMOMETER,
-                unit_of_measurement=UNIT_CELSIUS,
-                accuracy_decimals=1,
-                device_class=DEVICE_CLASS_TEMPERATURE,
-                state_class=STATE_CLASS_MEASUREMENT,
-            ),
-            cv.Optional(CONF_HOTEND_TEMPERATURE): sensor.sensor_schema(
-                unit_of_measurement=UNIT_CELSIUS,
-                icon = ICON_THERMOMETER,
-                accuracy_decimals=1,
-                device_class=DEVICE_CLASS_TEMPERATURE,
-                state_class=STATE_CLASS_MEASUREMENT,
-            ),
-            cv.Optional(CONF_AIR_FLOW_RATE): sensor.sensor_schema(
-                unit_of_measurement=UNIT_METER_PER_SECOND,
-                icon = ICON_WEATHER_WINDY,
-                accuracy_decimals=1,
-                device_class=DEVICE_CLASS_WIND_SPEED,
-                state_class=STATE_CLASS_MEASUREMENT,
-            ),
-            cv.Optional(CONF_MIN_AIR_FLOW_RATE): sensor.sensor_schema(
-                unit_of_measurement=UNIT_METER_PER_SECOND,
-                icon = ICON_WEATHER_WINDY,
-                accuracy_decimals=1,
-                device_class=DEVICE_CLASS_WIND_SPEED,
-                state_class=STATE_CLASS_MEASUREMENT,
-            ),
-            cv.Optional(CONF_MAX_AIR_FLOW_RATE): sensor.sensor_schema(
-                unit_of_measurement=UNIT_METER_PER_SECOND,
-                icon = ICON_WEATHER_WINDY,
-                accuracy_decimals=1,
-                device_class=DEVICE_CLASS_WIND_SPEED,
-                state_class=STATE_CLASS_MEASUREMENT,
-            ),
-            cv.Optional(CONF_HEAT_POWER): sensor.sensor_schema(
-                unit_of_measurement=UNIT_WATT,
-                icon = ICON_HEATING_COIL,
-                accuracy_decimals=3,
-                device_class=DEVICE_CLASS_POWER,
-                state_class=STATE_CLASS_MEASUREMENT,
-            ),
-            cv.Optional(CONF_AIR_CONSUMPTION): sensor.sensor_schema(
-                unit_of_measurement=UNIT_CUBIC_METER_PER_HOUR,
-                icon = ICON_WEATHER_WINDY,
-                accuracy_decimals=3,
-                device_class=DEVICE_CLASS_VOLUME,
-                state_class=STATE_CLASS_MEASUREMENT,
-            ),
-            cv.Optional(CONF_FIRMWARE_VERSION): sensor.sensor_schema(
-    		    unit_of_measurement=UNIT_EMPTY,
-        		icon=ICON_CHIP,
-        		accuracy_decimals=1,
-        		state_class=STATE_CLASS_MEASUREMENT,
-        		device_class=DEVICE_CLASS_EMPTY,
-    		    entity_category=ENTITY_CATEGORY_DIAGNOSTIC,
-            ),
-
-
-            cv.Optional(CONF_STATUS_UP): binary_sensor.binary_sensor_schema(
-                device_class=DEVICE_CLASS_PROBLEM,
-            ),
-            cv.Optional(CONF_STATUS_OV): binary_sensor.binary_sensor_schema(
-                device_class=DEVICE_CLASS_PROBLEM,
-            ),
-            cv.Optional(CONF_STATUS_WDT): binary_sensor.binary_sensor_schema(
-                device_class=DEVICE_CLASS_PROBLEM,
-            ),
-
-        }
-    )
+    cv.Schema({
+        cv.GenerateID(): cv.declare_id(CGAnemComponent),
+        cv.Optional(CONF_DUCT, default=91.6): cv.float_range(min=1, max=1000),
+        
+        cv.Optional(CONF_AMBIENT_TEMPERATURE): sensor.sensor_schema(
+            icon=ICON_THERMOMETER,
+            unit_of_measurement=UNIT_CELSIUS,
+            accuracy_decimals=1,
+            device_class=DEVICE_CLASS_TEMPERATURE,
+            state_class=STATE_CLASS_MEASUREMENT,
+        ),
+        cv.Optional(CONF_HOTEND_TEMPERATURE): sensor.sensor_schema(
+            unit_of_measurement=UNIT_CELSIUS,
+            icon=ICON_THERMOMETER,
+            accuracy_decimals=1,
+            device_class=DEVICE_CLASS_TEMPERATURE,
+            state_class=STATE_CLASS_MEASUREMENT,
+        ),
+        cv.Optional(CONF_AIR_FLOW_RATE): sensor.sensor_schema(
+            unit_of_measurement=UNIT_METER_PER_SECOND,
+            icon=ICON_WEATHER_WINDY,
+            accuracy_decimals=1,
+            device_class=DEVICE_CLASS_WIND_SPEED,
+            state_class=STATE_CLASS_MEASUREMENT,
+        ),
+        cv.Optional(CONF_MIN_AIR_FLOW_RATE): sensor.sensor_schema(
+            unit_of_measurement=UNIT_METER_PER_SECOND,
+            icon=ICON_WEATHER_WINDY,
+            accuracy_decimals=1,
+            device_class=DEVICE_CLASS_WIND_SPEED,
+            state_class=STATE_CLASS_MEASUREMENT,
+        ),
+        cv.Optional(CONF_MAX_AIR_FLOW_RATE): sensor.sensor_schema(
+            unit_of_measurement=UNIT_METER_PER_SECOND,
+            icon=ICON_WEATHER_WINDY,
+            accuracy_decimals=1,
+            device_class=DEVICE_CLASS_WIND_SPEED,
+            state_class=STATE_CLASS_MEASUREMENT,
+        ),
+        cv.Optional(CONF_HEAT_POWER): sensor.sensor_schema(
+            unit_of_measurement=UNIT_WATT,
+            icon=ICON_HEATING_COIL,
+            accuracy_decimals=3,
+            device_class=DEVICE_CLASS_POWER,
+            state_class=STATE_CLASS_MEASUREMENT,
+        ),
+        cv.Optional(CONF_AIR_CONSUMPTION): sensor.sensor_schema(
+            unit_of_measurement=UNIT_CUBIC_METER_PER_HOUR,
+            icon=ICON_WEATHER_WINDY,
+            accuracy_decimals=3,
+            device_class=DEVICE_CLASS_VOLUME,
+            state_class=STATE_CLASS_MEASUREMENT,
+        ),
+        cv.Optional(CONF_FIRMWARE_VERSION): sensor.sensor_schema(
+            unit_of_measurement=UNIT_EMPTY,
+            icon=ICON_CHIP,
+            accuracy_decimals=1,
+            state_class=STATE_CLASS_MEASUREMENT,
+            device_class=DEVICE_CLASS_EMPTY,
+            entity_category=ENTITY_CATEGORY_DIAGNOSTIC,
+        ),
+        cv.Optional(CONF_STATUS_UP): binary_sensor.binary_sensor_schema(
+            device_class=DEVICE_CLASS_PROBLEM,
+        ),
+        cv.Optional(CONF_STATUS_OV): binary_sensor.binary_sensor_schema(
+            device_class=DEVICE_CLASS_PROBLEM,
+        ),
+        cv.Optional(CONF_STATUS_WDT): binary_sensor.binary_sensor_schema(
+            device_class=DEVICE_CLASS_PROBLEM,
+        ),
+    })
     .extend(cv.polling_component_schema("1s"))
     .extend(i2c.i2c_device_schema(0x11))
 )
-
 
 async def to_code(config):
     var = cg.new_Pvariable(config[CONF_ID])
     await cg.register_component(var, config)
     await i2c.register_i2c_device(var, config)
-    cg.add(var.set_duct(config[CONF_DUCT]))
+    
+    if CONF_DUCT in config:
+        cg.add(var.set_duct(config[CONF_DUCT]))
+    
+    # Сенсоры
     if CONF_AMBIENT_TEMPERATURE in config:
         sens = await sensor.new_sensor(config[CONF_AMBIENT_TEMPERATURE])
         cg.add(var.set_ambient_temperature_sensor(sens))
+        
     if CONF_HOTEND_TEMPERATURE in config:
         sens = await sensor.new_sensor(config[CONF_HOTEND_TEMPERATURE])
         cg.add(var.set_hotend_temperature_sensor(sens))
+        
     if CONF_AIR_FLOW_RATE in config:
         sens = await sensor.new_sensor(config[CONF_AIR_FLOW_RATE])
         cg.add(var.set_air_flow_rate_sensor(sens))
+        
     if CONF_MIN_AIR_FLOW_RATE in config:
         sens = await sensor.new_sensor(config[CONF_MIN_AIR_FLOW_RATE])
         cg.add(var.set_min_air_flow_rate_sensor(sens))
+        
     if CONF_MAX_AIR_FLOW_RATE in config:
         sens = await sensor.new_sensor(config[CONF_MAX_AIR_FLOW_RATE])
         cg.add(var.set_max_air_flow_rate_sensor(sens))
+        
     if CONF_HEAT_POWER in config:
         sens = await sensor.new_sensor(config[CONF_HEAT_POWER])
         cg.add(var.set_heat_power_sensor(sens))
+        
     if CONF_FIRMWARE_VERSION in config:
         sens = await sensor.new_sensor(config[CONF_FIRMWARE_VERSION])
         cg.add(var.set_firmware_version_sensor(sens))
+        
     if CONF_AIR_CONSUMPTION in config:
         sens = await sensor.new_sensor(config[CONF_AIR_CONSUMPTION])
         cg.add(var.set_air_consumption_sensor(sens))
-
-
+    
+    # Бинарные сенсоры
     if CONF_STATUS_UP in config:
         sens = await binary_sensor.new_binary_sensor(config[CONF_STATUS_UP])
         cg.add(var.set_status_up_binary_sensor(sens))
+        
     if CONF_STATUS_OV in config:
         sens = await binary_sensor.new_binary_sensor(config[CONF_STATUS_OV])
         cg.add(var.set_status_ov_binary_sensor(sens))
+        
     if CONF_STATUS_WDT in config:
         sens = await binary_sensor.new_binary_sensor(config[CONF_STATUS_WDT])
         cg.add(var.set_status_wdt_binary_sensor(sens))
