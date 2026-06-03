52febb369e125200cabf512ad55da9f846800c42
ananyevgv <81970899+ananyevgv@users.noreply.github.com>
2026-06-03 14:58:27 +0300

    Update number.py

--- components/rad_sens/number.py
+++ components/rad_sens/number.py
@@ -1,32 +1,30 @@
 import esphome.codegen as cg
 import esphome.config_validation as cv
 from esphome.components import number
-from esphome.const import (
-    CONF_ID,
-    CONF_ICON,
-    CONF_UNIT_OF_MEASUREMENT,
-    ICON_TUNE,
-    UNIT_EMPTY,
-)
 
-from . import RadSensComponent, CONF_SENSITIVITY_NUMBER
+from . import RadSensComponent
 
 DEPENDENCIES = ['rad_sens']
 
 CONFIG_SCHEMA = cv.Schema({
-    cv.Required(CONF_ID): cv.use_id(RadSensComponent),
-    cv.Optional(CONF_SENSITIVITY_NUMBER): number.NUMBER_SCHEMA.extend({
-        cv.Optional(CONF_ICON, default=ICON_TUNE): cv.icon,
-        cv.Optional(CONF_UNIT_OF_MEASUREMENT, default="imp/µR"): cv.string_strict,
+    cv.GenerateID(): cv.use_id(RadSensComponent),
+    cv.Optional("sensitivity"): number.NUMBER_SCHEMA.extend({
+        cv.Optional("min_value", default=100): cv.float_,
+        cv.Optional("max_value", default=1100): cv.float_,
+        cv.Optional("step", default=1): cv.float_,
+        cv.Optional("unit_of_measurement", default="imp/µR"): cv.string_strict,
+        cv.Optional("icon", default="mdi:tune"): cv.icon,
     }),
 })
 
 async def to_code(config):
     parent = await cg.get_variable(config[CONF_ID])
     
-    if CONF_SENSITIVITY_NUMBER in config:
-        num = await number.new_number(config[CONF_SENSITIVITY_NUMBER],
-                                     min_value=100,
-                                     max_value=1100,
-                                     step=10)
+    if "sensitivity" in config:
+        num = await number.new_number(
+            config["sensitivity"],
+            min_value=config["sensitivity"]["min_value"],
+            max_value=config["sensitivity"]["max_value"],
+            step=config["sensitivity"]["step"]
+        )
         cg.add(parent.set_sensitivity_number(num))
