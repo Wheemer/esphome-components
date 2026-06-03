712f1185c31d30be7b288a8f4b7389857f18fdd1
ananyevgv <81970899+ananyevgv@users.noreply.github.com>
2026-06-03 14:57:26 +0300

    Create switch.py

---
+++ components/rad_sens/switch.py
@@ -0,0 +1,19 @@
+import esphome.codegen as cg
+import esphome.config_validation as cv
+from esphome.components import switch
+
+from . import RadSensComponent
+
+DEPENDENCIES = ['rad_sens']
+
+CONFIG_SCHEMA = cv.Schema({
+    cv.GenerateID(): cv.use_id(RadSensComponent),
+    cv.Optional("hv_generator_switch"): switch.switch_schema(),
+})
+
+async def to_code(config):
+    parent = await cg.get_variable(config[CONF_ID])
+    
+    if "hv_generator_switch" in config:
+        sw = await switch.new_switch(config["hv_generator_switch"])
+        cg.add(parent.set_hv_generator_switch(sw))
