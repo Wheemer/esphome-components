e463c7886955b6fd10eb3b773f76fa01f659b7d9
ananyevgv <81970899+ananyevgv@users.noreply.github.com>
2026-06-03 15:27:17 +0300

    Update switch.py

--- components/rad_sens/switch.py
+++ components/rad_sens/switch.py
@@ -10,6 +10,8 @@ DEPENDENCIES = ['rad_sens']
 CONFIG_SCHEMA = cv.Schema({
     cv.GenerateID(): cv.use_id(RadSensComponent),
     cv.Optional("hv_generator_switch"): switch.switch_schema(class_=switch.Switch),
+    cv.Optional("led_switch"): switch.switch_schema(class_=switch.Switch),
+    cv.Optional("low_power_switch"): switch.switch_schema(class_=switch.Switch),
 })
 
 async def to_code(config):
@@ -18,3 +20,11 @@ async def to_code(config):
     if "hv_generator_switch" in config:
         sw = await switch.new_switch(config["hv_generator_switch"])
         cg.add(parent.set_hv_generator_switch(sw))
+    
+    if "led_switch" in config:
+        sw = await switch.new_switch(config["led_switch"])
+        cg.add(parent.set_led_switch(sw))
+    
+    if "low_power_switch" in config:
+        sw = await switch.new_switch(config["low_power_switch"])
+        cg.add(parent.set_low_power_switch(sw))
