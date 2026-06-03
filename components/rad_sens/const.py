c365f9c8da9924fb311ae454f44837685da83d16
ananyevgv <81970899+ananyevgv@users.noreply.github.com>
2026-06-03 14:48:57 +0300

    Create const.py

---
+++ components/rad_sens/const.py
@@ -0,0 +1,8 @@
+UNIT_MICROROENTGEN_PER_HOUR = "µR/h"
+UNIT_COUNT_PER_MINUTE = "count/min"
+
+# Имена конфигураций
+CONF_DYNAMIC_INTENSITY = "dynamic_intensity"
+CONF_STATIC_INTENSITY = "static_intensity"
+CONF_COUNTS_PER_MINUTE = "pulses"
+CONF_FIRMWARE_VERSION = "firmware_version"
