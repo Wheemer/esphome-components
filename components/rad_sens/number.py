03b6c0c07448c454cb6106179ac93e25603347da
ananyevgv <81970899+ananyevgv@users.noreply.github.com>
2026-06-03 15:48:43 +0300

    Update number.py

--- components/rad_sens/number.py
+++ components/rad_sens/number.py
@@ -7,31 +7,26 @@ from . import RadSensComponent
 
 DEPENDENCIES = ['rad_sens']
 
+CONFIG_SCHEMA = cv.Schema({
+    cv.GenerateID(): cv.use_id(RadSensComponent),
+    cv.Optional("sensitivity"): number._NUMBER_SCHEMA,
+    cv.Optional("min_value", default=100): cv.float_,
+    cv.Optional("max_value", default=1100): cv.float_,
+})
+
 async def to_code(config):
     parent = await cg.get_variable(config[CONF_ID])
     
     if "sensitivity" in config:
         sens_config = config["sensitivity"]
         
-        # Создаём number с авто-генерируемым ID
-        num = cg.new_Pvariable(cg.global_id("rad_sens_sens"))
-        cg.add(num.set_min_value(sens_config["min_value"]))
-        cg.add(num.set_max_value(sens_config["max_value"]))
-        cg.add(num.set_step(sens_config["step"]))
-        cg.add(num.set_unit_of_measurement(sens_config["unit_of_measurement"]))
-        cg.add(num.set_icon(sens_config["icon"]))
+        # Используем стандартную функцию new_number
+        # Она сама создаст ID, если его нет
+        num = await number.new_number(
+            sens_config,
+            min_value=100,
+            max_value=1100,
+            step=1
+        )
         
-        await number.register_number(num, sens_config)
         cg.add(parent.set_sensitivity_number(num))
-
-CONFIG_SCHEMA = cv.Schema({
-    cv.GenerateID(): cv.use_id(RadSensComponent),
-    cv.Optional("sensitivity"): cv.Schema({
-        cv.Optional("name"): cv.string,
-        cv.Optional("min_value", default=100): cv.float_,
-        cv.Optional("max_value", default=1100): cv.float_,
-        cv.Optional("step", default=1): cv.float_,
-        cv.Optional("unit_of_measurement", default="imp/µR"): cv.string,
-        cv.Optional("icon", default="mdi:tune"): cv.icon,
-    }),
-})
