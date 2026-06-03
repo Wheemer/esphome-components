9ecfa90d343779395b6fa78f0de2b4cfeb2494ac
ananyevgv <81970899+ananyevgv@users.noreply.github.com>
2026-06-03 14:48:17 +0300

    Update __init__.py

--- components/rad_sens/__init__.py
+++ components/rad_sens/__init__.py
@@ -8,10 +8,20 @@ from esphome.const import (
     CONF_STEP,
     CONF_MIN_VALUE,
     CONF_MAX_VALUE,
+    CONF_ICON,
+    CONF_UNIT_OF_MEASUREMENT,
     ICON_RADIOACTIVE,
     STATE_CLASS_MEASUREMENT,
-    UNIT_MICROSIEMENS_PER_HOUR,
-    UNIT_COUNT,
+)
+
+# Импортируем константы из локального файла
+from .const import (
+    UNIT_MICROROENTGEN_PER_HOUR,
+    UNIT_COUNT_PER_MINUTE,
+    CONF_DYNAMIC_INTENSITY,
+    CONF_STATIC_INTENSITY,
+    CONF_COUNTS_PER_MINUTE,
+    CONF_FIRMWARE_VERSION
 )
 
 DEPENDENCIES = ['i2c']
@@ -20,23 +30,22 @@ AUTO_LOAD = ['sensor', 'binary_sensor', 'switch', 'number']
 rad_sens_ns = cg.esphome_ns.namespace('rad_sens')
 RadSensComponent = rad_sens_ns.class_('RadSensComponent', i2c.I2CDevice, cg.Component)
 
-CONF_DYNAMIC_INTENSITY = "dynamic_intensity"
-CONF_STATIC_INTENSITY = "static_intensity"
-CONF_PULSES = "pulses"
+# Используем импортированные константы
+CONF_PULSES = CONF_COUNTS_PER_MINUTE
 CONF_HV_GENERATOR_STATE = "hv_generator_state"
 CONF_HV_GENERATOR_SWITCH = "hv_generator_switch"
 CONF_SENSITIVITY_NUMBER = "sensitivity"
 
-# Конфигурация сенсоров - убрали device_class
+# Конфигурация сенсоров
 RAD_SENS_SENSOR_SCHEMA = sensor.sensor_schema(
-    unit_of_measurement=UNIT_MICROSIEMENS_PER_HOUR,
+    unit_of_measurement=UNIT_MICROROENTGEN_PER_HOUR,
     icon=ICON_RADIOACTIVE,
     accuracy_decimals=3,
     state_class=STATE_CLASS_MEASUREMENT,
 )
 
 RAD_SENS_PULSES_SCHEMA = sensor.sensor_schema(
-    unit_of_measurement=UNIT_COUNT,
+    unit_of_measurement=UNIT_COUNT_PER_MINUTE,
     icon=ICON_RADIOACTIVE,
     accuracy_decimals=0,
     state_class=STATE_CLASS_MEASUREMENT,
@@ -47,6 +56,8 @@ RAD_SENS_SENSITIVITY_NUMBER_SCHEMA = number.NUMBER_SCHEMA.extend({
     cv.Optional(CONF_MIN_VALUE, default=100): cv.float_,
     cv.Optional(CONF_MAX_VALUE, default=1100): cv.float_,
     cv.Optional(CONF_STEP, default=1): cv.float_,
+    cv.Optional(CONF_UNIT_OF_MEASUREMENT, default="imp/µR"): cv.string_strict,
+    cv.Optional(CONF_ICON, default="mdi:tune"): cv.icon,
 })
 
 # Схема конфигурации
