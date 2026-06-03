e593c95874d261252b018e87d5759b3dab1b247f
ananyevgv <81970899+ananyevgv@users.noreply.github.com>
2026-06-03 13:59:23 +0300

    Update LilygotBattery.h

--- components/lilygo_t_battery/LilygotBattery.h
+++ components/lilygo_t_battery/LilygotBattery.h
@@ -17,10 +17,12 @@ class LilygotBattery : public PollingComponent {
   sensor::Sensor *bus_voltage{nullptr};
   sensor::Sensor *battery_level{nullptr};
 
-  // Настройки делителя напряжения (по умолчанию для TTGO T-Display)
   void set_voltage_divider(float divider) { voltage_divider_ = divider; }
   void set_enable_pin(GPIOPin *enable) { enable_pin_ = enable; }
   void set_adc_sensor(sensor::Sensor *sensor) { adc_sensor_ = sensor; }
+  void set_voltage_sensor(sensor::Sensor *sensor) { voltage = sensor; }
+  void set_bus_voltage_sensor(sensor::Sensor *sensor) { bus_voltage = sensor; }
+  void set_battery_level_sensor(sensor::Sensor *sensor) { battery_level = sensor; }
   
   void setup() override;
   void update() override;
@@ -29,7 +31,7 @@ class LilygotBattery : public PollingComponent {
  protected:
   sensor::Sensor *adc_sensor_{nullptr};
   GPIOPin *enable_pin_{nullptr};
-  float voltage_divider_ = 6.0f;  // (120k + 20k) / 20k = 6
+  float voltage_divider_ = 6.0f;
   float reference_voltage_ = 3.3f;
   
   float read_adc_voltage_();
@@ -37,9 +39,10 @@ class LilygotBattery : public PollingComponent {
   int calculate_battery_level_(float battery_voltage, bool is_usb_present);
   bool is_usb_present_();
   
-  // Калибровка ADC для ESP-IDF
+#ifdef USE_ESP32
   esp_adc_cal_characteristics_t adc_chars_;
   bool adc_calibrated_ = false;
+#endif
 };
 
 }  // namespace lilygo_t_battery
