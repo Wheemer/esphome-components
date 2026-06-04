ed4e1d9a34b2925eee26a8268ce94f1dca43b72f
ananyevgv <81970899+ananyevgv@users.noreply.github.com>
2026-06-04 15:48:39 +0300

    Update water_heater.py

--- components/polaris_kettle/water_heater.py
+++ components/polaris_kettle/water_heater.py
@@ -1,29 +1,19 @@
 import esphome.codegen as cg
 import esphome.config_validation as cv
-from esphome.components import uart, water_heater
-from esphome.const import CONF_ID, CONF_UART_ID
+from esphome.components import water_heater
+from esphome.const import CONF_ID
 
-DEPENDENCIES = ["uart"]
-AUTO_LOAD = ["water_heater"]
+from . import PolarisKettle, polaris_kettle_ns
 
-polaris_kettle_ns = cg.esphome_ns.namespace("polaris_kettle")
-PolarisKettle = polaris_kettle_ns.class_(
-    "PolarisKettle", water_heater.WaterHeater, cg.Component, uart.UARTDevice
+DEPENDENCIES = ["polaris_kettle"]
+
+PolarisWaterHeater = polaris_kettle_ns.class_(
+    "PolarisKettle", water_heater.WaterHeater
 )
 
-CONFIG_SCHEMA = (
-    cv.Schema({
-        cv.GenerateID(CONF_ID): cv.declare_id(PolarisKettle),
-        cv.GenerateID(CONF_UART_ID): cv.use_id(uart.UARTComponent),
-    })
-    .extend(water_heater.WATER_HEATER_SCHEMA)
-    .extend(cv.COMPONENT_SCHEMA)
-)
+CONFIG_SCHEMA = water_heater.WATER_HEATER_SCHEMA.extend({
+    cv.GenerateID(CONF_ID): cv.use_id(PolarisKettle),
+}).extend(cv.COMPONENT_SCHEMA)
 
 async def to_code(config):
-    var = cg.new_Pvariable(config[CONF_ID])
-    await cg.register_component(var, config)
-    await water_heater.register_water_heater(var, config)
-    
-    uart_parent = await cg.get_variable(config[CONF_UART_ID])
-    cg.add(var.set_uart_parent(uart_parent))
+    parent = await cg.get_variable(config[CONF_ID])
