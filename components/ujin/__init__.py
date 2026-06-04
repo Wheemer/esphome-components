8ddd6fd9595850eb75827e1eda7f9c8faf1a9f9e
ananyevgv <81970899+ananyevgv@users.noreply.github.com>
2026-06-04 15:28:32 +0300

    Update __init__.py

--- components/ujin/__init__.py
+++ components/ujin/__init__.py
@@ -11,15 +11,11 @@ UjinComponent = ujin_ns.class_("UjinComponent", cg.Component, uart.UARTDevice)
 
 CONF_UJIN_ID = "ujin_id"
 CONF_ON_STATE = "on_state"
-CONF_ON_SETTINGS_CHANGED = "on_settings_changed"
 
 CONFIG_SCHEMA = cv.Schema({
     cv.GenerateID(): cv.declare_id(UjinComponent),
     cv.Required(CONF_UART_ID): cv.use_id(uart.UARTComponent),
     cv.Optional(CONF_ON_STATE): cv.automation({
-        cv.GenerateID(CONF_UJIN_ID): cv.use_id(UjinComponent),
-    }),
-    cv.Optional(CONF_ON_SETTINGS_CHANGED): cv.automation({
         cv.GenerateID(CONF_UJIN_ID): cv.use_id(UjinComponent),
     }),
 }).extend(cv.COMPONENT_SCHEMA)
@@ -31,17 +27,9 @@ async def to_code(config):
     uart_component = await cg.get_variable(config[CONF_UART_ID])
     cg.add(var.set_uart_parent(uart_component))
     
-    # Регистрируем автоматизации
     for conf in config.get(CONF_ON_STATE, []):
         await cg.register_automation(conf, (
             (cg.RawExpression(f"(({var})->get_state_callback().add"), 
-             cg.RawExpression(")"),
-             cg.RawExpression("void(std::shared_ptr<esphome::ujin::DeviceState>)"))
-        ))
-    
-    for conf in config.get(CONF_ON_SETTINGS_CHANGED, []):
-        await cg.register_automation(conf, (
-            (cg.RawExpression(f"(({var})->get_settings_callback().add"), 
              cg.RawExpression(")"),
              cg.RawExpression("void(std::shared_ptr<esphome::ujin::DeviceState>)"))
         ))
