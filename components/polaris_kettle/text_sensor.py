017e6c4de3c9a80cf52ccd159f14cafad46f8612
ananyevgv <81970899+ananyevgv@users.noreply.github.com>
2026-06-05 15:53:16 +0300

    Update text_sensor.py

--- components/polaris_kettle/text_sensor.py
+++ components/polaris_kettle/text_sensor.py
@@ -3,23 +3,25 @@ import esphome.config_validation as cv
 from esphome.components import text_sensor
 from esphome.const import CONF_ID
 
-from . import polaris_kettle_ns, PolarisKettle
+from . import polaris_kettle_ns, PolarisKettle, CONF_POLARIS_KETTLE_ID
 
 DEPENDENCIES = ["polaris_kettle"]
 
-PolarisModeTextSensor = polaris_kettle_ns.class_(
-    "PolarisModeTextSensor", text_sensor.TextSensor, cg.Component
+PolarisTextSensor = polaris_kettle_ns.class_(
+    "PolarisTextSensor", text_sensor.TextSensor, cg.Component
 )
 
-CONF_POLARIS_KETTLE_ID = "polaris_kettle_id"
-
-MODE_SCHEMA = text_sensor.text_sensor_schema(
+CONFIG_SCHEMA = text_sensor.text_sensor_schema(
     icon="mdi:kettle"
 ).extend({
+    cv.GenerateID(CONF_ID): cv.declare_id(PolarisTextSensor),
     cv.GenerateID(CONF_POLARIS_KETTLE_ID): cv.use_id(PolarisKettle),
-})
+}).extend(cv.COMPONENT_SCHEMA)
 
-async def register_mode_text_sensor(var, config):
+async def to_code(config):
+    var = cg.new_Pvariable(config[CONF_ID])
+    await cg.register_component(var, config)
+    
     parent = await cg.get_variable(config[CONF_POLARIS_KETTLE_ID])
     cg.add(var.set_parent(parent))
     cg.add(parent.register_mode_text_sensor(var))
