2dbb260fb4c4bf01b9480c905fc195d05ce9c636
ananyevgv <81970899+ananyevgv@users.noreply.github.com>
2026-06-05 15:33:10 +0300

    Update __init__.py

--- components/ujin/__init__.py
+++ components/ujin/__init__.py
@@ -2,6 +2,7 @@ import esphome.codegen as cg
 import esphome.config_validation as cv
 from esphome.const import CONF_ID, CONF_UART_ID
 from esphome.components import uart
+from esphome.core import CORE, coroutine_with_priority
 
 CODEOWNERS = ["@ujin"]
 DEPENDENCIES = ["uart"]
@@ -15,11 +16,12 @@ CONF_ON_STATE = "on_state"
 CONFIG_SCHEMA = cv.Schema({
     cv.GenerateID(): cv.declare_id(UjinComponent),
     cv.Required(CONF_UART_ID): cv.use_id(uart.UARTComponent),
-    cv.Optional(CONF_ON_STATE): cv.automation({
+    cv.Optional(CONF_ON_STATE): cv.validate_automation({
         cv.GenerateID(CONF_UJIN_ID): cv.use_id(UjinComponent),
     }),
 }).extend(cv.COMPONENT_SCHEMA)
 
+@coroutine_with_priority(100.0)
 async def to_code(config):
     var = cg.new_Pvariable(config[CONF_ID])
     await cg.register_component(var, config)
@@ -28,8 +30,5 @@ async def to_code(config):
     cg.add(var.set_uart_parent(uart_component))
     
     for conf in config.get(CONF_ON_STATE, []):
-        await cg.register_automation(conf, (
-            (cg.RawExpression(f"(({var})->get_state_callback().add"), 
-             cg.RawExpression(")"),
-             cg.RawExpression("void(std::shared_ptr<esphome::ujin::DeviceState>)"))
-        ))
+        trigger = cg.new_Pvariable(conf[CONF_ID], var)
+        await cg.register_automation(trigger, conf)
