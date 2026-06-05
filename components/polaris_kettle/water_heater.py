f05d28acc336059c01cabbf905a65cdac7f0d5cf
ananyevgv <81970899+ananyevgv@users.noreply.github.com>
2026-06-05 16:32:11 +0300

    Update water_heater.py

--- components/polaris_kettle/water_heater.py
+++ components/polaris_kettle/water_heater.py
@@ -3,14 +3,17 @@ import esphome.config_validation as cv
 from esphome.components import water_heater
 from esphome.const import CONF_ID
 
-from . import polaris_kettle_ns, PolarisKettle, CONF_POLARIS_KETTLE_ID
+from . import polaris_ns
+from .const import CONF_POLARIS_ID
 
-DEPENDENCIES = ["polaris_kettle"]
+DEPENDENCIES = ["polaris"]
+
+PolarisWaterHeater = polaris_ns.class_("PolarisKettle", water_heater.WaterHeater)
 
 CONFIG_SCHEMA = water_heater.WATER_HEATER_SCHEMA.extend({
-    cv.GenerateID(CONF_POLARIS_KETTLE_ID): cv.use_id(PolarisKettle),
+    cv.GenerateID(CONF_POLARIS_ID): cv.use_id(PolarisWaterHeater),
 }).extend(cv.COMPONENT_SCHEMA)
 
 async def to_code(config):
-    # Водонагреватель уже зарегистрирован
-    pass
+    parent = await cg.get_variable(config[CONF_POLARIS_ID])
+    # Water heater уже зарегистрирован в основном компоненте
