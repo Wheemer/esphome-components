29db00358060fc820e8e4af148e802e75e247e46
ananyevgv <81970899+ananyevgv@users.noreply.github.com>
2026-06-05 16:07:45 +0300

    Update polaris_kettle.h

--- components/polaris_kettle/polaris_kettle.h
+++ components/polaris_kettle/polaris_kettle.h
@@ -1,3 +1,5 @@
+#pragma once
+
 #include "esphome.h"
 #include "esphome/components/water_heater/water_heater.h"
 #include "esphome/components/uart/uart.h"
@@ -10,30 +12,24 @@
 namespace esphome {
 namespace polaris_kettle {
 
-// Forward declarations
-class PolarisSensor;
-class PolarisTextSensor;
-class PolarisBinarySensor;
-class PolarisButton;
-
 class PolarisKettle : public water_heater::WaterHeater, public Component, public uart::UARTDevice {
  public:
   PolarisKettle() = default;
   
   void set_uart_parent(uart::UARTComponent *parent) {
     parent_ = parent;
-    this->set_uart(parent);
+    this->uart_ = parent;
   }
   
   // Регистрация сенсоров
-  void register_current_sensor(PolarisSensor *sensor) { current_sensor_ = sensor; }
-  void register_target_sensor(PolarisSensor *sensor) { target_sensor_ = sensor; }
-  void register_mode_text_sensor(PolarisTextSensor *sensor) { mode_text_sensor_ = sensor; }
-  void register_no_kettle_sensor(PolarisBinarySensor *sensor) { no_kettle_sensor_ = sensor; }
-  void register_no_water_sensor(PolarisBinarySensor *sensor) { no_water_sensor_ = sensor; }
-  void register_button(PolarisButton *button, const std::string &type);
+  void register_current_sensor(sensor::Sensor *sensor) { current_sensor_ = sensor; }
+  void register_target_sensor(sensor::Sensor *sensor) { target_sensor_ = sensor; }
+  void register_mode_text_sensor(text_sensor::TextSensor *sensor) { mode_text_sensor_ = sensor; }
+  void register_no_kettle_sensor(binary_sensor::BinarySensor *sensor) { no_kettle_sensor_ = sensor; }
+  void register_no_water_sensor(binary_sensor::BinarySensor *sensor) { no_water_sensor_ = sensor; }
+  void register_button(button::Button *button, const std::string &type);
   
-  // Публичные методы
+  // Публичные методы для кнопок
   void black_tea() { send_preset(95, 0x3D); }
   void mix_tea()   { send_preset(40, 0x3B); }
   void white_tea() { send_preset(65, 0x3C); }
@@ -42,30 +38,32 @@ class PolarisKettle : public water_heater::WaterHeater, public Component, public
   void bag_tea()   { send_preset(100, 0x7E); }
   void boil()      { send_preset(100, 0x3B); }
   void keep_warm() { send_preset(40, 0x63); }
-  void turn_off()  { send_command(0x00, 0x00, 0x00, 0x00, 0x00); }
-  void set_temperature(float temp) { send_target_temperature((uint8_t)temp); }
-  
-  // Геттеры
-  float get_current_temperature() const { return current_temp_; }
   
   void setup() override;
   void loop() override;
   void control(const water_heater::WaterHeaterCall &call) override;
-  void publish_state() override;
+  
+  // Геттеры для сенсоров
+  float get_current_temp() const { return current_temp_; }
+  float get_target_temp() const { return target_temperature_; }
+  std::string get_mode_text() const { return mode_text_; }
+  bool get_no_kettle() const { return no_kettle_; }
+  bool get_no_water() const { return no_water_; }
 
  protected:
   uart::UARTComponent *parent_{nullptr};
   std::vector<uint8_t> buffer_;
   float current_temp_ = 0.0;
+  float target_temperature_ = 70.0;
   bool no_kettle_ = false;
   bool no_water_ = false;
   std::string mode_text_ = "Выключен";
   
-  PolarisSensor *current_sensor_{nullptr};
-  PolarisSensor *target_sensor_{nullptr};
-  PolarisTextSensor *mode_text_sensor_{nullptr};
-  PolarisBinarySensor *no_kettle_sensor_{nullptr};
-  PolarisBinarySensor *no_water_sensor_{nullptr};
+  sensor::Sensor *current_sensor_{nullptr};
+  sensor::Sensor *target_sensor_{nullptr};
+  text_sensor::TextSensor *mode_text_sensor_{nullptr};
+  binary_sensor::BinarySensor *no_kettle_sensor_{nullptr};
+  binary_sensor::BinarySensor *no_water_sensor_{nullptr};
   
   void process_frame();
   uint16_t calculate_checksum(uint8_t *data, int len);
@@ -76,40 +74,7 @@ class PolarisKettle : public water_heater::WaterHeater, public Component, public
   void update_sensors();
 };
 
-// Вспомогательные классы
-class PolarisSensor : public sensor::Sensor, public Component {
- public:
-  void set_parent(PolarisKettle *parent) { parent_ = parent; }
-  void setup() override {}
-  void update() override {}
-  void loop() override {}
-  void publish(float value) { publish_state(value); }
- protected:
-  PolarisKettle *parent_;
-};
-
-class PolarisTextSensor : public text_sensor::TextSensor, public Component {
- public:
-  void set_parent(PolarisKettle *parent) { parent_ = parent; }
-  void setup() override {}
-  void update() override {}
-  void loop() override {}
-  void publish(const std::string &value) { publish_state(value); }
- protected:
-  PolarisKettle *parent_;
-};
-
-class PolarisBinarySensor : public binary_sensor::BinarySensor, public Component {
- public:
-  void set_parent(PolarisKettle *parent) { parent_ = parent; }
-  void setup() override {}
-  void update() override {}
-  void loop() override {}
-  void publish(bool value) { publish_state(value); }
- protected:
-  PolarisKettle *parent_;
-};
-
+// Вспомогательный класс для кнопок
 class PolarisButton : public button::Button, public Component {
  public:
   void set_parent(PolarisKettle *parent) { parent_ = parent; }
