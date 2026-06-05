1776c792e0e83fa287de02f2adfbc31a38a70863
ananyevgv <81970899+ananyevgv@users.noreply.github.com>
2026-06-05 16:29:52 +0300

    Create const.py

---
+++ components/polaris_kettle/const.py
@@ -0,0 +1,20 @@
+POLARIS = "polaris"
+
+CONF_POLARIS_ID = "polaris_id"
+CONF_SYNC_MODE = "sync_mode"
+
+# Типы компонентов
+SENSOR = "sensor"
+TEXT_SENSOR = "text_sensor"
+BINARY_SENSOR = "binary_sensor"
+BUTTON = "button"
+
+# Команды для чайной церемонии
+CMD_BLACK_TEA = 0x3D  # 95°C
+CMD_MIX_TEA = 0x3B    # 40°C
+CMD_WHITE_TEA = 0x3C  # 65°C
+CMD_GREEN_TEA = 0x7C  # 80°C
+CMD_OOLONG_TEA = 0x7D # 90°C
+CMD_BAG_TEA = 0x7E    # 100°C
+CMD_BOIL = 0x3B       # Кипячение
+CMD_KEEP_WARM = 0x63  # Подогрев
