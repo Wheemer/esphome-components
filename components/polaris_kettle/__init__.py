18fea72f91f68573e9d9955a596e93144bfdec0c
ananyevgv <81970899+ananyevgv@users.noreply.github.com>
2026-06-05 16:29:24 +0300

    Update __init__.py

--- components/polaris_kettle/__init__.py
+++ components/polaris_kettle/__init__.py
@@ -3,16 +3,17 @@ import esphome.config_validation as cv
 from esphome.components import uart
 from esphome.const import CONF_ID, CONF_UART_ID
 
-DEPENDENCIES = ["uart"]
+from . import const, generate, schema, validate
 
-polaris_kettle_ns = cg.esphome_ns.namespace("polaris_kettle")
-PolarisKettle = polaris_kettle_ns.class_(
-    "PolarisKettle", cg.Component, uart.UARTDevice
-)
+DEPENDENCIES = ["uart"]
+AUTO_LOAD = ["water_heater"]
+CODEOWNERS = ["@yourname"]
+MULTI_CONF = True
 
 CONFIG_SCHEMA = cv.Schema({
-    cv.GenerateID(CONF_ID): cv.declare_id(PolarisKettle),
-    cv.GenerateID(CONF_UART_ID): cv.use_id(uart.UARTComponent),
+    cv.GenerateID(CONF_ID): cv.declare_id(generate.PolarisKettle),
+    cv.Required(CONF_UART_ID): cv.use_id(uart.UARTComponent),
+    cv.Optional(const.CONF_SYNC_MODE, False): cv.boolean,
 }).extend(cv.COMPONENT_SCHEMA)
 
 async def to_code(config):
@@ -21,3 +22,6 @@ async def to_code(config):
     
     uart_parent = await cg.get_variable(config[CONF_UART_ID])
     cg.add(var.set_uart_parent(uart_parent))
+    
+    if const.CONF_SYNC_MODE in config:
+        cg.add(var.set_sync_mode(config[const.CONF_SYNC_MODE]))
