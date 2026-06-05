9747ba584e89f603b37a2acf2ccd19793997e7a6
ananyevgv <81970899+ananyevgv@users.noreply.github.com>
2026-06-05 16:37:52 +0300

    Update sensor.py

--- components/polaris_kettle/sensor.py
+++ components/polaris_kettle/sensor.py
@@ -3,36 +3,28 @@ import esphome.config_validation as cv
 from esphome.components import sensor
 from esphome.const import CONF_ID, CONF_TYPE
 
-from . import polaris_kettle_ns, PolarisKettle, CONF_POLARIS_KETTLE_ID
+from . import generate
+from .const import CONF_POLARIS_ID
 
-DEPENDENCIES = ["polaris_kettle"]
-
-PolarisSensor = polaris_kettle_ns.class_(
-    "PolarisSensor", sensor.Sensor, cg.Component
-)
+DEPENDENCIES = ["polaris"]
 
 CONF_SENSOR_TYPE = "type"
-SENSOR_TYPES = {
-    "current": "current",
-    "target": "target",
-}
+SENSOR_TYPES = ["current", "target"]
 
-# Используем _SENSOR_SCHEMA
-CONFIG_SCHEMA = sensor._SENSOR_SCHEMA.extend({
-    cv.GenerateID(CONF_ID): cv.declare_id(PolarisSensor),
-    cv.GenerateID(CONF_POLARIS_KETTLE_ID): cv.use_id(PolarisKettle),
-    cv.Required(CONF_SENSOR_TYPE): cv.enum(SENSOR_TYPES, lower=True),
+CONFIG_SCHEMA = sensor.sensor_schema().extend({
+    cv.GenerateID(CONF_ID): cv.declare_id(sensor.Sensor),
+    cv.GenerateID(CONF_POLARIS_ID): cv.use_id(generate.PolarisKettle),
+    cv.Required(CONF_SENSOR_TYPE): cv.one_of(*SENSOR_TYPES, lower=True),
 }).extend(cv.COMPONENT_SCHEMA)
 
 async def to_code(config):
-    var = cg.new_Pvariable(config[CONF_ID])
+    var = await sensor.new_sensor(config)
     await cg.register_component(var, config)
     
-    parent = await cg.get_variable(config[CONF_POLARIS_KETTLE_ID])
-    cg.add(var.set_parent(parent))
+    parent = await cg.get_variable(config[CONF_POLARIS_ID])
     
     sensor_type = config[CONF_SENSOR_TYPE]
     if sensor_type == "current":
-        cg.add(parent.register_current_sensor(var))
+        cg.add(parent.set_current_temperature_sensor(var))
     else:
-        cg.add(parent.register_target_sensor(var))
+        cg.add(parent.set_target_temperature_sensor(var))
