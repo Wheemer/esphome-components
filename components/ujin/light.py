164fb025901969589f349c6746844ce593da7872
ananyevgv <81970899+ananyevgv@users.noreply.github.com>
2026-06-05 15:33:57 +0300

    Update light.py

--- components/ujin/light.py
+++ components/ujin/light.py
@@ -1,7 +1,7 @@
 import esphome.codegen as cg
 import esphome.config_validation as cv
 from esphome.components import light
-from esphome.const import CONF_OUTPUT_ID, CONF_NAME, CONF_ID
+from esphome.const import CONF_OUTPUT_ID, CONF_ID
 from . import ujin_ns, UjinComponent, CONF_UJIN_ID
 
 UjinLight = ujin_ns.class_("UjinLight", light.LightOutput, cg.Component)
@@ -12,7 +12,7 @@ CONFIG_SCHEMA = light.LIGHT_SCHEMA.extend({
     cv.GenerateID(CONF_OUTPUT_ID): cv.declare_id(UjinLight),
     cv.GenerateID(CONF_UJIN_ID): cv.use_id(UjinComponent),
     cv.Required(CONF_CHANNEL): cv.int_range(min=1, max=2),
-}).extend(cv.COMPONENT_SCHEMA)
+})
 
 async def to_code(config):
     var = cg.new_Pvariable(config[CONF_OUTPUT_ID])
