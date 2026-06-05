8fbb7700cdff162e55652bf0ea0e8b0343c8c933
ananyevgv <81970899+ananyevgv@users.noreply.github.com>
2026-06-05 15:59:56 +0300

    Update water_heater.py

--- components/polaris_kettle/water_heater.py
+++ components/polaris_kettle/water_heater.py
@@ -7,10 +7,11 @@ from . import polaris_kettle_ns, PolarisKettle, CONF_POLARIS_KETTLE_ID
 
 DEPENDENCIES = ["polaris_kettle"]
 
+# Для water_heater используем стандартный подход
 CONFIG_SCHEMA = water_heater.WATER_HEATER_SCHEMA.extend({
     cv.GenerateID(CONF_POLARIS_KETTLE_ID): cv.use_id(PolarisKettle),
 }).extend(cv.COMPONENT_SCHEMA)
 
 async def to_code(config):
     parent = await cg.get_variable(config[CONF_POLARIS_KETTLE_ID])
-    cg.add(parent.set_water_heater_parent(config))
+    # Водонагреватель уже зарегистрирован в основном компоненте
