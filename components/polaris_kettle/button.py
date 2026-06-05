625b29f23dae7f27b9601cc565bf2865d55e6a7d
ananyevgv <81970899+ananyevgv@users.noreply.github.com>
2026-06-05 16:38:57 +0300

    Update button.py

--- components/polaris_kettle/button.py
+++ components/polaris_kettle/button.py
@@ -3,38 +3,32 @@ import esphome.config_validation as cv
 from esphome.components import button
 from esphome.const import CONF_ID, CONF_TYPE
 
-from . import polaris_kettle_ns, PolarisKettle, CONF_POLARIS_KETTLE_ID
+from . import generate, polaris_ns
+from .const import CONF_POLARIS_ID
 
-DEPENDENCIES = ["polaris_kettle"]
+DEPENDENCIES = ["polaris"]
 
-PolarisButton = polaris_kettle_ns.class_(
-    "PolarisButton", button.Button, cg.Component
-)
+PolarisButton = polaris_ns.class_("PolarisButton", button.Button, cg.Component)
 
 CONF_BUTTON_TYPE = "type"
-BUTTON_TYPES = {
-    "black_tea": "black_tea",
-    "mix_tea": "mix_tea",
-    "white_tea": "white_tea",
-    "green_tea": "green_tea",
-    "oolong_tea": "oolong_tea",
-    "bag_tea": "bag_tea",
-    "boil": "boil",
-    "keep_warm": "keep_warm",
-}
+BUTTON_TYPES = [
+    "black_tea", "mix_tea", "white_tea", "green_tea",
+    "oolong_tea", "bag_tea", "boil", "keep_warm"
+]
 
-CONFIG_SCHEMA = button._BUTTON_SCHEMA.extend({
+CONFIG_SCHEMA = button.button_schema(PolarisButton).extend({
     cv.GenerateID(CONF_ID): cv.declare_id(PolarisButton),
-    cv.GenerateID(CONF_POLARIS_KETTLE_ID): cv.use_id(PolarisKettle),
-    cv.Required(CONF_BUTTON_TYPE): cv.enum(BUTTON_TYPES, lower=True),
+    cv.GenerateID(CONF_POLARIS_ID): cv.use_id(generate.PolarisKettle),
+    cv.Required(CONF_BUTTON_TYPE): cv.one_of(*BUTTON_TYPES, lower=True),
 }).extend(cv.COMPONENT_SCHEMA)
 
 async def to_code(config):
     var = cg.new_Pvariable(config[CONF_ID])
     await cg.register_component(var, config)
     
-    parent = await cg.get_variable(config[CONF_POLARIS_KETTLE_ID])
+    parent = await cg.get_variable(config[CONF_POLARIS_ID])
     cg.add(var.set_parent(parent))
     
     button_type = config[CONF_BUTTON_TYPE]
     cg.add(var.set_type(button_type))
+    cg.add(parent.set_button(var, button_type))
