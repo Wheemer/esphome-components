4f7b188090bf1d6d9e4435380146364f1293493b
ananyevgv <81970899+ananyevgv@users.noreply.github.com>
2026-06-05 16:39:16 +0300

    Update generate.py

--- components/polaris_kettle/generate.py
+++ components/polaris_kettle/generate.py
@@ -1,13 +1,4 @@
 import esphome.codegen as cg
 
-from . import const
-
 polaris_ns = cg.esphome_ns.namespace("polaris")
 PolarisKettle = polaris_ns.class_("PolarisKettle", cg.Component, cg.uart.UARTDevice)
-PolarisWaterHeater = polaris_ns.class_("PolarisWaterHeater", cg.Component)
-
-def define_has_component(component_type: str, keys: list[str]) -> None:
-    cg.add_define(
-        f"POLARIS_{component_type.upper()}_LIST(F, sep)",
-        cg.RawExpression(" sep ".join(f"F({key})" for key in keys))
-    )
