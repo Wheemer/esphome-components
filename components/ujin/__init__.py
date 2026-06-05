234ab1d23064e07000e0a9e4b17688b1aafa1c45
ananyevgv <81970899+ananyevgv@users.noreply.github.com>
2026-06-05 15:40:56 +0300

    Update __init__.py

--- components/ujin/__init__.py
+++ components/ujin/__init__.py
@@ -1,6 +1,5 @@
 import esphome.codegen as cg
 import esphome.config_validation as cv
-import esphome.automation as automation
 from esphome.const import CONF_ID, CONF_UART_ID
 from esphome.components import uart
 
@@ -13,11 +12,10 @@ UjinComponent = ujin_ns.class_("UjinComponent", cg.Component, uart.UARTDevice)
 CONF_UJIN_ID = "ujin_id"
 CONF_ON_STATE = "on_state"
 
-# Автоматизация с использованием callback-метода (рекомендованный подход [citation:5])
 CONFIG_SCHEMA = cv.Schema({
     cv.GenerateID(): cv.declare_id(UjinComponent),
     cv.Required(CONF_UART_ID): cv.use_id(uart.UARTComponent),
-    cv.Optional(CONF_ON_STATE): automation.validate_automation({
+    cv.Optional(CONF_ON_STATE): cv.build_automation({
         cv.GenerateID(CONF_UJIN_ID): cv.use_id(UjinComponent),
     }),
 }).extend(cv.COMPONENT_SCHEMA)
@@ -29,11 +27,5 @@ async def to_code(config):
     uart_component = await cg.get_variable(config[CONF_UART_ID])
     cg.add(var.set_uart_parent(uart_component))
     
-    # Создание автоматизации с использованием build_callback_automation [citation:5]
     for conf in config.get(CONF_ON_STATE, []):
-        await automation.build_callback_automation(
-            var,
-            "add_on_state_callback",
-            [(ujin_ns.struct("DeviceState"), "state")],
-            conf
-        )
+        await cg.register_automation(conf, None, var.get_state_trigger())
