cbc5801e73c6610dc2a8e78673d493262a5a5995
ananyevgv <81970899+ananyevgv@users.noreply.github.com>
2026-06-05 16:38:40 +0300

    Update binary_sensor.py

--- components/polaris_kettle/binary_sensor.py
+++ components/polaris_kettle/binary_sensor.py
@@ -3,35 +3,28 @@ import esphome.config_validation as cv
 from esphome.components import binary_sensor
 from esphome.const import CONF_ID, CONF_TYPE
 
-from . import polaris_kettle_ns, PolarisKettle, CONF_POLARIS_KETTLE_ID
+from . import generate
+from .const import CONF_POLARIS_ID
 
-DEPENDENCIES = ["polaris_kettle"]
-
-PolarisBinarySensor = polaris_kettle_ns.class_(
-    "PolarisBinarySensor", binary_sensor.BinarySensor, cg.Component
-)
+DEPENDENCIES = ["polaris"]
 
 CONF_SENSOR_TYPE = "type"
-SENSOR_TYPES = {
-    "no_kettle": "no_kettle",
-    "no_water": "no_water",
-}
+SENSOR_TYPES = ["no_kettle", "no_water"]
 
 CONFIG_SCHEMA = binary_sensor.binary_sensor_schema().extend({
-    cv.GenerateID(CONF_ID): cv.declare_id(PolarisBinarySensor),
-    cv.GenerateID(CONF_POLARIS_KETTLE_ID): cv.use_id(PolarisKettle),
-    cv.Required(CONF_SENSOR_TYPE): cv.enum(SENSOR_TYPES, lower=True),
+    cv.GenerateID(CONF_ID): cv.declare_id(binary_sensor.BinarySensor),
+    cv.GenerateID(CONF_POLARIS_ID): cv.use_id(generate.PolarisKettle),
+    cv.Required(CONF_SENSOR_TYPE): cv.one_of(*SENSOR_TYPES, lower=True),
 }).extend(cv.COMPONENT_SCHEMA)
 
 async def to_code(config):
-    var = cg.new_Pvariable(config[CONF_ID])
+    var = await binary_sensor.new_binary_sensor(config)
     await cg.register_component(var, config)
     
-    parent = await cg.get_variable(config[CONF_POLARIS_KETTLE_ID])
-    cg.add(var.set_parent(parent))
+    parent = await cg.get_variable(config[CONF_POLARIS_ID])
     
     sensor_type = config[CONF_SENSOR_TYPE]
     if sensor_type == "no_kettle":
-        cg.add(parent.register_no_kettle_sensor(var))
+        cg.add(parent.set_no_kettle_binary_sensor(var))
     else:
-        cg.add(parent.register_no_water_sensor(var))
+        cg.add(parent.set_no_water_binary_sensor(var))
