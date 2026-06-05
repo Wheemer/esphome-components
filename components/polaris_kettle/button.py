97f01fa2bd1af23f29607376ecadc45a36a10dd6
ananyevgv <81970899+ananyevgv@users.noreply.github.com>
2026-06-05 15:53:54 +0300

    Update button.py

--- components/polaris_kettle/button.py
+++ components/polaris_kettle/button.py
@@ -1,63 +1,40 @@
 import esphome.codegen as cg
 import esphome.config_validation as cv
 from esphome.components import button
-from esphome.const import CONF_ID
+from esphome.const import CONF_ID, CONF_TYPE
 
-from . import polaris_kettle_ns, PolarisKettle
+from . import polaris_kettle_ns, PolarisKettle, CONF_POLARIS_KETTLE_ID
 
 DEPENDENCIES = ["polaris_kettle"]
 
-BlackTeaButton = polaris_kettle_ns.class_("BlackTeaButton", button.Button, cg.Component)
-MixTeaButton = polaris_kettle_ns.class_("MixTeaButton", button.Button, cg.Component)
-WhiteTeaButton = polaris_kettle_ns.class_("WhiteTeaButton", button.Button, cg.Component)
-GreenTeaButton = polaris_kettle_ns.class_("GreenTeaButton", button.Button, cg.Component)
-OolongTeaButton = polaris_kettle_ns.class_("OolongTeaButton", button.Button, cg.Component)
-BagTeaButton = polaris_kettle_ns.class_("BagTeaButton", button.Button, cg.Component)
-BoilButton = polaris_kettle_ns.class_("BoilButton", button.Button, cg.Component)
-KeepWarmButton = polaris_kettle_ns.class_("KeepWarmButton", button.Button, cg.Component)
+PolarisButton = polaris_kettle_ns.class_(
+    "PolarisButton", button.Button, cg.Component
+)
 
-CONF_POLARIS_KETTLE_ID = "polaris_kettle_id"
+CONF_BUTTON_TYPE = "type"
+BUTTON_TYPES = {
+    "black_tea": "black_tea",
+    "mix_tea": "mix_tea",
+    "white_tea": "white_tea",
+    "green_tea": "green_tea",
+    "oolong_tea": "oolong_tea",
+    "bag_tea": "bag_tea",
+    "boil": "boil",
+    "keep_warm": "keep_warm",
+}
 
-BUTTON_SCHEMA = button._BUTTON_SCHEMA.extend({
+CONFIG_SCHEMA = button.button_schema().extend({
+    cv.GenerateID(CONF_ID): cv.declare_id(PolarisButton),
     cv.GenerateID(CONF_POLARIS_KETTLE_ID): cv.use_id(PolarisKettle),
+    cv.Required(CONF_BUTTON_TYPE): cv.enum(BUTTON_TYPES, lower=True),
 }).extend(cv.COMPONENT_SCHEMA)
 
-async def register_black_tea_button(var, config):
+async def to_code(config):
+    var = cg.new_Pvariable(config[CONF_ID])
+    await cg.register_component(var, config)
+    
     parent = await cg.get_variable(config[CONF_POLARIS_KETTLE_ID])
     cg.add(var.set_parent(parent))
-    cg.add(parent.register_black_tea_button(var))
-
-async def register_mix_tea_button(var, config):
-    parent = await cg.get_variable(config[CONF_POLARIS_KETTLE_ID])
-    cg.add(var.set_parent(parent))
-    cg.add(parent.register_mix_tea_button(var))
-
-async def register_white_tea_button(var, config):
-    parent = await cg.get_variable(config[CONF_POLARIS_KETTLE_ID])
-    cg.add(var.set_parent(parent))
-    cg.add(parent.register_white_tea_button(var))
-
-async def register_green_tea_button(var, config):
-    parent = await cg.get_variable(config[CONF_POLARIS_KETTLE_ID])
-    cg.add(var.set_parent(parent))
-    cg.add(parent.register_green_tea_button(var))
-
-async def register_oolong_tea_button(var, config):
-    parent = await cg.get_variable(config[CONF_POLARIS_KETTLE_ID])
-    cg.add(var.set_parent(parent))
-    cg.add(parent.register_oolong_tea_button(var))
-
-async def register_bag_tea_button(var, config):
-    parent = await cg.get_variable(config[CONF_POLARIS_KETTLE_ID])
-    cg.add(var.set_parent(parent))
-    cg.add(parent.register_bag_tea_button(var))
-
-async def register_boil_button(var, config):
-    parent = await cg.get_variable(config[CONF_POLARIS_KETTLE_ID])
-    cg.add(var.set_parent(parent))
-    cg.add(parent.register_boil_button(var))
-
-async def register_keep_warm_button(var, config):
-    parent = await cg.get_variable(config[CONF_POLARIS_KETTLE_ID])
-    cg.add(var.set_parent(parent))
-    cg.add(parent.register_keep_warm_button(var))
+    
+    button_type = config[CONF_BUTTON_TYPE]
+    cg.add(parent.register_button(var, button_type))
