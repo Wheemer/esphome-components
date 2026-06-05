0bc5052b632aefc65d27524238a3f591a4fdfb89
ananyevgv <81970899+ananyevgv@users.noreply.github.com>
2026-06-05 15:52:59 +0300

    Update sensor.py

--- components/polaris_kettle/sensor.py
+++ components/polaris_kettle/sensor.py
@@ -1,44 +1,41 @@
 import esphome.codegen as cg
 import esphome.config_validation as cv
 from esphome.components import sensor
-from esphome.const import CONF_ID
+from esphome.const import CONF_ID, CONF_TYPE
 
-from . import polaris_kettle_ns, PolarisKettle
+from . import polaris_kettle_ns, PolarisKettle, CONF_POLARIS_KETTLE_ID
 
 DEPENDENCIES = ["polaris_kettle"]
 
-PolarisCurrentTemperatureSensor = polaris_kettle_ns.class_(
-    "PolarisCurrentTemperatureSensor", sensor.Sensor, cg.Component
+PolarisSensor = polaris_kettle_ns.class_(
+    "PolarisSensor", sensor.Sensor, cg.Component
 )
 
-PolarisTargetTemperatureSensor = polaris_kettle_ns.class_(
-    "PolarisTargetTemperatureSensor", sensor.Sensor, cg.Component
-)
+CONF_SENSOR_TYPE = "type"
+SENSOR_TYPES = {
+    "current": "current",
+    "target": "target",
+}
 
-CONF_POLARIS_KETTLE_ID = "polaris_kettle_id"
-
-CURRENT_TEMP_SCHEMA = sensor.sensor_schema(
+CONFIG_SCHEMA = sensor.sensor_schema(
     unit_of_measurement="°C",
     accuracy_decimals=1,
     icon="mdi:thermometer"
 ).extend({
+    cv.GenerateID(CONF_ID): cv.declare_id(PolarisSensor),
     cv.GenerateID(CONF_POLARIS_KETTLE_ID): cv.use_id(PolarisKettle),
-})
+    cv.Required(CONF_SENSOR_TYPE): cv.enum(SENSOR_TYPES, lower=True),
+}).extend(cv.COMPONENT_SCHEMA)
 
-TARGET_TEMP_SCHEMA = sensor.sensor_schema(
-    unit_of_measurement="°C",
-    accuracy_decimals=1,
-    icon="mdi:thermometer"
-).extend({
-    cv.GenerateID(CONF_POLARIS_KETTLE_ID): cv.use_id(PolarisKettle),
-})
-
-async def register_current_temp_sensor(var, config):
+async def to_code(config):
+    var = cg.new_Pvariable(config[CONF_ID])
+    await cg.register_component(var, config)
+    
     parent = await cg.get_variable(config[CONF_POLARIS_KETTLE_ID])
     cg.add(var.set_parent(parent))
-    cg.add(parent.register_current_temperature_sensor(var))
-
-async def register_target_temp_sensor(var, config):
-    parent = await cg.get_variable(config[CONF_POLARIS_KETTLE_ID])
-    cg.add(var.set_parent(parent))
-    cg.add(parent.register_target_temperature_sensor(var))
+    
+    sensor_type = config[CONF_SENSOR_TYPE]
+    if sensor_type == "current":
+        cg.add(parent.register_current_sensor(var))
+    else:
+        cg.add(parent.register_target_sensor(var))
