e538ffc1e487745923f2185c5abe38426ba9be01
ananyevgv <81970899+ananyevgv@users.noreply.github.com>
2026-06-24 11:57:34 +0300

    Delete components/rad_sens directory

--- components/rad_sens/const.py
+++
@@ -1,9 +0,0 @@
-# Единицы измерения
-UNIT_MICROROENTGEN_PER_HOUR = "µR/h"
-UNIT_COUNT_PER_MINUTE = "count/min"
-
-# Имена конфигураций
-CONF_DYNAMIC_INTENSITY = "dynamic_intensity"
-CONF_STATIC_INTENSITY = "static_intensity"
-CONF_COUNTS_PER_MINUTE = "pulses"
-CONF_FIRMWARE_VERSION = "firmware_version"
